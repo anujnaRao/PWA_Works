67fb7195c6e91be3b271cb564f6b3f75b3058125
Anujna <anujna.prao5974@yahoo.com>
2020-04-27 08:52:56 +0530

    further updates

--- WebPage/public/js/app.js
+++ WebPage/public/js/app.js
@@ -7,3 +7,40 @@ if('serviceWorker' in navigator){
         console.error(err);
     })
 }
+
+
+let fetchIMGButton = document.getElementById('display');
+fetchIMGButton.addEventListener('click', fetchImgFunction);
+
+function fetchImgFunction(){
+    fetch('./img/pictures/mysore-palace.jpg')
+    .then( result =>{
+        return result.blob()
+    })
+    .then(res =>{
+        let container = document.getElementById('dispaly');
+        let img = document.createElement('img');
+        container.appendChild(img);
+        const imgURL = URL.createObjectURL(res);
+        img.src = imgURL;
+    })
+    .catch(err =>{
+        console.error(err);
+    })
+}
+
+let fetchTEXTButton = document.getElementById('fetchtext');
+fetchTEXTButton.addEventListener('click', fetchTextFunction);
+
+function fetchTextFunction(){
+    fetch('./text.txt')
+    .then( result =>{
+        return result.text()
+    })
+    .then(res =>{
+        console.log(res);
+    })
+    .catch(err =>{
+        console.error(err);
+    })
+}
